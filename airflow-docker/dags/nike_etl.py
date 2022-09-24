d8b7a53233bf7805687b3704aa61943aca558061
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-09-24 19:07:23 +0000

    edit Nike dir

--- airflow-docker/dags/nike_etl.py
+++ airflow-docker/dags/nike_etl.py
@@ -119,75 +119,76 @@ def transform(file):
         with open(file, "r") as f:
             data = json.load(f)
 
-        search_term = data["data"]["products"]["pages"]["searchSummary"].get("originalTerms")
+        if data["data"]["products"]["pages"]is not None:
+            search_term = data["data"]["products"]["pages"]["searchSummary"].get("originalTerms")
+        if data["data"]["products"]["products"] is not None:
+            for product in data["data"]["products"]["products"]:
+                id = product.get("id")
+                pid = product.get("pid")
+                product_id = product.get("cloudProductId")
+                product_instance_id = product.get("productInstanceId")
+                product_type = product.get("producType")
+                title = product.get("title")
+                subtitle = product.get("subtitle")
+                color_description = product.get("colorDescription")
+                currency = product["price"].get("currency")
+                current_price = product["price"].get("currentPrice")
+                discounted = product["price"].get("discounted")
+                employee_price = product["price"].get("employeePrice")
+                full_price = product["price"].get("fullPrice")
+                minimum_advertised_price = product["price"].get("minimumAdvertisedPrice")
+                label = product.get("label")
+                in_stock = product.get("inStock")
+                is_coming_soon = product.get("isComingSoon")
+                is_best_seller = product.get("isBestSeller")
+                is_excluded = product.get("isExcluded")
+                is_gift_card = product.get("isGiftCard")
+                is_jersey = product.get("isJersey")
+                is_launch = product.get("isLaunch")
+                is_member_exclusive = product.get("isMemberExclusive")
+                is_nba = product.get("isNBA")
+                is_nfl = product.get("isNFL")
+                is_sustainable = product.get("isSustainable")
+                has_extended_sizing = product.get("hasExtendedSizing")
+                customizable = product.get("customizable")
+                portrait_url = product["images"].get("portraitURL")
+                squarish_url = product["images"].get("squarishURL")
+                url = product.get("url")
 
-        for product in data["data"]["products"]["products"]:
-            id = product.get("id")
-            pid = product.get("pid")
-            product_id = product.get("cloudProductId")
-            product_instance_id = product.get("productInstanceId")
-            product_type = product.get("producType")
-            title = product.get("title")
-            subtitle = product.get("subtitle")
-            color_description = product.get("colorDescription")
-            currency = product["price"].get("currency")
-            current_price = product["price"].get("currentPrice")
-            discounted = product["price"].get("discounted")
-            employee_price = product["price"].get("employeePrice")
-            full_price = product["price"].get("fullPrice")
-            minimum_advertised_price = product["price"].get("minimumAdvertisedPrice")
-            label = product.get("label")
-            in_stock = product.get("inStock")
-            is_coming_soon = product.get("isComingSoon")
-            is_best_seller = product.get("isBestSeller")
-            is_excluded = product.get("isExcluded")
-            is_gift_card = product.get("isGiftCard")
-            is_jersey = product.get("isJersey")
-            is_launch = product.get("isLaunch")
-            is_member_exclusive = product.get("isMemberExclusive")
-            is_nba = product.get("isNBA")
-            is_nfl = product.get("isNFL")
-            is_sustainable = product.get("isSustainable")
-            has_extended_sizing = product.get("hasExtendedSizing")
-            customizable = product.get("customizable")
-            portrait_url = product["images"].get("portraitURL")
-            squarish_url = product["images"].get("squarishURL")
-            url = product.get("url")
-
-            yield (
-                id,
-                pid,
-                product_id,
-                product_instance_id,
-                product_type,
-                title,
-                subtitle,
-                color_description,
-                currency,
-                current_price,
-                discounted,
-                employee_price,
-                full_price,
-                minimum_advertised_price,
-                label,
-                in_stock,
-                is_coming_soon,
-                is_best_seller,
-                is_excluded,
-                is_gift_card,
-                is_jersey,
-                is_launch,
-                is_member_exclusive,
-                is_nba,
-                is_nfl,
-                is_sustainable,
-                has_extended_sizing,
-                customizable,
-                search_term,
-                portrait_url,
-                squarish_url,
-                url
-            )
+                yield (
+                    id,
+                    pid,
+                    product_id,
+                    product_instance_id,
+                    product_type,
+                    title,
+                    subtitle,
+                    color_description,
+                    currency,
+                    current_price,
+                    discounted,
+                    employee_price,
+                    full_price,
+                    minimum_advertised_price,
+                    label,
+                    in_stock,
+                    is_coming_soon,
+                    is_best_seller,
+                    is_excluded,
+                    is_gift_card,
+                    is_jersey,
+                    is_launch,
+                    is_member_exclusive,
+                    is_nba,
+                    is_nfl,
+                    is_sustainable,
+                    has_extended_sizing,
+                    customizable,
+                    search_term,
+                    portrait_url,
+                    squarish_url,
+                    url
+                )
     except BaseException as e:
         print(e)
 
@@ -206,24 +207,24 @@ create_table_query = """
     color_description VARCHAR(255),
     currency VARCHAR(20),
     current_price NUMERIC(12, 3),
-    discounted BOOLEAN,
+    discounted VARCHAR(5),
     employee_price NUMERIC(12, 3),
     full_price NUMERIC(12, 3),
     minimum_advertised_price NUMERIC(12, 3),
     label VARCHAR(40),
-    in_stock BOOLEAN,
-    is_coming_soon BOOLEAN,
-    is_best_seller BOOLEAN,
-    is_excluded BOOLEAN,
-    is_gift_card BOOLEAN,
-    is_jersey BOOLEAN,
-    is_launch BOOLEAN,
-    is_member_exclusive BOOLEAN,
-    is_nba BOOLEAN,
-    is_nfl BOOLEAN,
-    is_sustainable BOOLEAN,
-    has_extended_sizing BOOLEAN,
-    customizable BOOLEAN,
+    in_stock VARCHAR(5),
+    is_coming_soon VARCHAR(5),
+    is_best_seller VARCHAR(5),
+    is_excluded VARCHAR(5),
+    is_gift_card VARCHAR(5),
+    is_jersey VARCHAR(5),
+    is_launch VARCHAR(5),
+    is_member_exclusive VARCHAR(5),
+    is_nba VARCHAR(5),
+    is_nfl VARCHAR(5),
+    is_sustainable VARCHAR(5),
+    has_extended_sizing VARCHAR(5),
+    customizable VARCHAR(5),
     search_term VARCHAR(40),
     portrait_url VARCHAR(255),
     squarish_url VARCHAR(255),
